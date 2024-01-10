025845dbfbcfbda5a06a989da3db9ef5cddfcb72
salahmohsen <salahmmohsen@gmail.com>
2024-01-10 20:17:09 +0200

    Brands Carousel Improvement

--- app/ui/brandsCarousel/brandsCarousel.jsx
+++ app/ui/brandsCarousel/brandsCarousel.jsx
@@ -18,20 +18,20 @@ export default function BrandsCarousel() {
 
   return (
     <div className=" inline-flex w-full flex-nowrap overflow-hidden  pt-16 [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
-      <ul className="animate-infinite-scroll flex items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8">
+      <ul className="flex animate-infinite-scroll items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8">
         {logos.map((logo, index) => (
           <li key={index}>
-            <Image src={logo.src} alt={logo.alt} height={40} />
+            <Image src={logo.src} alt={logo.alt} height={40} priority={true} />
           </li>
         ))}
       </ul>
       <ul
-        className="animate-infinite-scroll flex items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8"
+        className="flex animate-infinite-scroll items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8"
         aria-hidden="true"
       >
         {logos.map((logo, index) => (
           <li key={index}>
-            <Image src={logo.src} alt={logo.alt} height={40} />
+            <Image src={logo.src} alt={logo.alt} height={40} priority={true} />
           </li>
         ))}
       </ul>
